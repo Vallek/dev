30155d473f956d2e3b29ec7ce2364f0d8c6d5142
Vallek <vforval86@gmail.com>
2023-04-09 18:44:01 +0300

    git init

---
+++ js/menu-popup.js
@@ -0,0 +1,25 @@
+const header = document.querySelector('.main-page');
+const headerMenu = document.querySelector('.header');
+const menuButton = document.querySelector('.popup-menu__button');
+const menuLink = document.querySelectorAll('.popup-menu__link');
+const firstLink = document.querySelector('.header__item:first-child');
+
+function scrollHeader() {
+	let pageTop = header.getBoundingClientRect().top;
+	if (pageTop < -600) {
+		headerMenu.classList.add('header__menu_anim');
+		firstLink.classList.remove('visually-hidden');
+	}
+	else {
+		headerMenu.classList.remove('header__menu_anim');
+		firstLink.classList.add('visually-hidden');
+	}	
+}
+
+function closeMenu() {
+	menuButton.click();
+}
+
+window.addEventListener('scroll', scrollHeader);
+window.addEventListener('DOMContentLoaded', scrollHeader);
+menuLink.forEach((el) => {el.addEventListener('click', closeMenu);});
