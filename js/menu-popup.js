a18d756177737c6c3551e79223d53c2987e389f5
Vallek <vforval86@gmail.com>
2024-05-04 15:28:04 +0300

    close menu on outside click

--- js/menu-popup.js
+++ js/menu-popup.js
@@ -1,25 +1,33 @@
-const header = document.querySelector('.page');
-const headerMenu = document.querySelector('.header');
-const menuButton = document.querySelector('.popup-menu__button');
-const menuLink = document.querySelectorAll('.popup-menu__link');
-const firstLink = document.querySelector('.header__item:first-child');
+const page = document.querySelector('.page');
+const header = document.querySelector('.header');
+const input = header.querySelector('.popup-menu__input');
+const menuLink = header.querySelectorAll('.popup-menu__link');
+const firstLink = header.querySelector('.header__item:first-child');
 
 function scrollHeader() {
-	let pageTop = header.getBoundingClientRect().top;
+	let pageTop = page.getBoundingClientRect().top;
 	if (pageTop < -600) {
-		headerMenu.classList.add('header__menu_anim');
+		header.classList.add('header__menu_anim');
 		firstLink.classList.remove('visually-hidden');
 	}
 	else {
-		headerMenu.classList.remove('header__menu_anim');
+		header.classList.remove('header__menu_anim');
 		firstLink.classList.add('visually-hidden');
 	}	
 }
 
-function closeMenu() {
-	menuButton.click();
-}
-
 window.addEventListener('scroll', scrollHeader);
 window.addEventListener('DOMContentLoaded', scrollHeader);
-menuLink.forEach((el) => {el.addEventListener('click', closeMenu);});
+
+// Close menu on link click to reveal content
+menuLink.forEach((el) => {el.addEventListener('click', () => {
+	input.checked = false;
+});});
+
+// Close when click outside
+document.addEventListener('click', (el) => {
+	let target = el.target;
+	if (!header.contains(target)) {
+		input.checked = false;
+	}
+});
